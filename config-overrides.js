15708d5c5ec582043ad837762c1c1403f694482a
HaiTS <transachhai97@gmail.com>
2020-07-29 20:52:26 +0700

    alias

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 module.exports = {
     webpack: function (config, env) {
+        config.resolve.alias = Object.assign(config.resolve.alias, {
+            '@': path.resolve(__dirname, './src'),
+        });
+
+        console.log(config.resolve.alias);
         return config;
     }
 };
