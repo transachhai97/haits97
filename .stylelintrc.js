7f7a2350a17f0a7b49ccb5c69fe2eb13bcb8780a
HaiTS <transachhai97@gmail.com>
2020-07-29 22:19:15 +0700

    stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "extends": [
+        "stylelint-config-airbnb",
+        "stylelint-config-rational-order"
+    ],
+    "plugins": [
+        "stylelint-order",
+        "stylelint-config-rational-order/plugin"
+    ],
+    "ignoreFiles": [
+        ".eslintrc.js",
+        "config-overrides.js"
+    ],
+    "rules": {
+        "indentation": 4,
+        "rule-empty-line-before": [
+            "always",
+            {
+                "except": [
+                    "first-nested"
+                ]
+            }
+        ],
+        "order/properties-order": [],
+        "plugin/rational-order": [
+            true,
+            {
+                "border-in-box-model": true,
+                "empty-line-between-groups": true
+            }
+        ]
+    }
+};
--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
     webpack: function (config, env) {
@@ -9,6 +10,12 @@ module.exports = {
         config.resolve.alias = Object.assign(config.resolve.alias, {
             '@': path.resolve(__dirname, './src'),
         });
+
+        config.plugins.push(
+            new StyleLintPlugin({
+                files: [`src/**/*.{js,jsx,htm,html,css,sss,less,scss,sass}`],
+            }),
+        );
 
         config.optimization.minimizer[0].options.extractComments = false;
         config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
