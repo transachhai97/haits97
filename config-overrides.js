bd49c0f4970217b5be6013b94908b74c0a52e17a
HaiTS <transachhai97@gmail.com>
2020-07-29 21:03:05 +0700

    source map

--- config-overrides.js
+++ config-overrides.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 module.exports = {
     webpack: function (config, env) {
+        const isProduction = env === 'production';
+
+        config.devtool = isProduction ? 'cheap-module-source-map' : 'source-map';
+
         config.resolve.alias = Object.assign(config.resolve.alias, {
             '@': path.resolve(__dirname, './src'),
         });
 
-        console.log(config.resolve.alias);
         return config;
     }
 };
