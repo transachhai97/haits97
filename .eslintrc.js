1a856572b16c8316977d3bf316a15064c687f08c
HaiTS <transachhai97@gmail.com>
2020-07-30 10:52:37 +0700

    eslint indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    commonjs: true,
-    node: true,
-  },
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-  ],
-  parser: 'babel-eslint',
-  rules: {
-    'no-console': 0,
-    'import/no-unresolved': 0,
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx'],
-      },
+    env: {
+        browser: true,
+        es6: true,
+        commonjs: true,
+        node: true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb/hooks',
     ],
-  },
+    parser: 'babel-eslint',
+    rules: {
+        'no-console': 0,
+        'import/no-unresolved': 0,
+        'react/jsx-filename-extension': [
+            1,
+            {
+                extensions: ['.js', '.jsx'],
+            },
+        ],
+        indent: ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+    },
 };
