dc50f3803237c1227f05170c1c8c6917bb4bd8d8
HaiTS <transachhai97@gmail.com>
2020-07-30 08:59:42 +0700

    build path

--- config-overrides.js
+++ config-overrides.js
@@ -6,6 +6,8 @@ module.exports = {
         const isProduction = env === 'production';
 
         config.devtool = isProduction ? 'cheap-module-source-map' : 'source-map';
+        config.optimization.runtimeChunk = false;
+        config.optimization.splitChunks = false;
 
         config.resolve.alias = Object.assign(config.resolve.alias, {
             '@': path.resolve(__dirname, './src'),
@@ -16,6 +18,11 @@ module.exports = {
                 files: [`src/**/*.{js,jsx,htm,html,css,sss,less,scss,sass}`],
             }),
         );
+
+        config.output.filename = 'static/js/[name].[contenthash:8].js';
+        config.plugins[4].options.filename = 'static/css/[name].[contenthash:8].css';
+        config.module.rules[2].oneOf[0].options.name = 'static/media/[name].[hash:8].[ext]';
+        config.module.rules[2].oneOf[7].options.name = 'static/media/[name].[hash:8].[ext]';
 
         config.optimization.minimizer[0].options.extractComments = false;
         config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
