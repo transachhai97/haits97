467bcda64bc95b0f6a1491edeb73fc2bfd2ee655
HaiTS <transachhai97@gmail.com>
2020-08-09 17:50:21 +0700

    loading

--- src/components/GlobalLoading/GlobalLoading.js
+++ src/components/GlobalLoading/GlobalLoading.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const GlobalLoading = () => <div>Loading...</div>;
+import IconLoading from '@/assets/icons/loading.svg';
+
+import stylesGlobalLoading from './css/GlobalLoading.scss';
+
+function GlobalLoading() {
+    return (
+        <div
+            className={stylesGlobalLoading['global-loading']}
+            style={{
+                background: `url(${IconLoading}) center no-repeat rgba(0,0,0,.8)`,
+            }}
+        />
+    );
+}
 
 export default GlobalLoading;
