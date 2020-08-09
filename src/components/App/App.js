0a49687e3c032e92c35c06d48dc689886b334fdd
HaiTS <transachhai97@gmail.com>
2020-08-09 21:02:44 +0700

    app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import logo from '@/logo.svg';
+import logo from '@/assets/images/logo.svg';
 import stylesApp from './css/App.scss';
 
 function App(props) {
