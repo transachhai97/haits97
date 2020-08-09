e88025d59bb62761654629bb6b910425c84e8892
HaiTS <transachhai97@gmail.com>
2020-08-09 16:32:59 +0700

    nprogress

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import 'react-app-polyfill/stable';
 
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+
+import CNprogress from '@/components/CNprogress/CNprogress';
+
 import '@/index.scss';
 
 const App = React.lazy(() => import('@/App'));
 
 ReactDOM.render(
     <React.StrictMode>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<CNprogress />}>
             <App name="Trần Sách Hải" />
         </Suspense>
     </React.StrictMode>,
