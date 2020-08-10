7ae0c24ed6729f394aa22403c38a30db43a1e5e8
HaiTS <transachhai97@gmail.com>
2020-08-10 14:56:02 +0700

    helmet

--- src/components/App/AppComponent.js
+++ src/components/App/AppComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Helmet } from 'react-helmet';
 
 import { defaultPropsApp, propTypesApp } from '@/components/App/App';
 
@@ -11,27 +12,32 @@ function AppComponent(props) {
     const { name } = props;
 
     return (
-        <div className={stylesApp.app}>
-            <header className={stylesApp.header}>
-                <img src={logo} className={stylesApp.logo} alt="logo" />
-                <p>
-                    Edit
-                    {' '}
-                    <code>src/App.js</code>
-                    {' '}
-                    and save to reload.
-                </p>
-                <a
-                    className={stylesApp.link}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <h1>{`${t('hello')} ${name}`}</h1>
-            </header>
-        </div>
+        <>
+            <Helmet>
+                <title>My Title</title>
+            </Helmet>
+            <div className={stylesApp.app}>
+                <header className={stylesApp.header}>
+                    <img src={logo} className={stylesApp.logo} alt="logo" />
+                    <p>
+                        Edit
+                        {' '}
+                        <code>src/App.js</code>
+                        {' '}
+                        and save to reload.
+                    </p>
+                    <a
+                        className={stylesApp.link}
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <h1>{`${t('hello')} ${name}`}</h1>
+                </header>
+            </div>
+        </>
     );
 }
 
